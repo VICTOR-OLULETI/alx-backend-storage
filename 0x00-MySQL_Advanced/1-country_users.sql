a85a158448a3c2d186b011b44af4a69fe6c07343
VICTOR-OLULETI <oluletiv@gmail.com>
2023-10-15 04:59:20 +0100

    task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 --Creates table users.ABORT
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+--DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
